03044c0c74fbd4ff693cc9b22c2d564548881c16
jiko <jiko@stardust7>
2023-05-02 18:40:23 +0200

    upd: add comments in hooks/post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,12 +11,19 @@ def remove_file(filepath):
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
+        print("="*77)
+        print("Removing AUTHORS.rst file.")
         remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
+        print("Removing docs/source/authors.rst file.")
+        remove_file('docs/source/authors.rst')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
+        print("="*77)
+        print("Removing cli.py file.")
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
+        print("="*77)
+        print("Removing LICENSE file.")
         remove_file('LICENSE')
